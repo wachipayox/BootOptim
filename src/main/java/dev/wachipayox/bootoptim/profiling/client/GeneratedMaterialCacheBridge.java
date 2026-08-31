b724f6c024adbe7962158b622f96722cc41f06ee
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-31 18:01:53 +0200

    Remove unused generated material cache bridge

--- src/main/java/dev/wachipayox/bootoptim/profiling/client/GeneratedMaterialCacheBridge.java
+++
@@ -1,6 +0,0 @@
-package dev.wachipayox.bootoptim.profiling.client;
-
-/** Internal bridge implemented by the diagnostic BlockModel mixin for generated-item models only. */
-public interface GeneratedMaterialCacheBridge {
-    void bootoptim$enableGeneratedMaterialCache();
-}
