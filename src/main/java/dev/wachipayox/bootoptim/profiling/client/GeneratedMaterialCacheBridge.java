041727da080b579d5d8b2c2533f5542d64d567ad
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-31 17:50:58 +0200

    Add generated model material cache bridge

---
+++ src/main/java/dev/wachipayox/bootoptim/profiling/client/GeneratedMaterialCacheBridge.java
@@ -0,0 +1,6 @@
+package dev.wachipayox.bootoptim.profiling.client;
+
+/** Internal bridge implemented by the diagnostic BlockModel mixin for generated-item models only. */
+public interface GeneratedMaterialCacheBridge {
+    void bootoptim$enableGeneratedMaterialCache();
+}
