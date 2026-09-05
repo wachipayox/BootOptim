34b0425d0f54bac429de4f6cd9e582235d6a1be2
Wachii <dani.alvarado.sosa@gmail.com>
2026-09-05 21:54:27 +0200

    Report indexed join verifier at title

--- src/main/java/dev/wachipayox/bootoptim/profiling/client/ClientStartupHooks.java
+++ src/main/java/dev/wachipayox/bootoptim/profiling/client/ClientStartupHooks.java
@@ -27,6 +27,7 @@ public final class ClientStartupHooks {
             return;
         }
 
+        VoxelShaperIndexedJoinVerifier.onMainMenu();
         if (StartupProfiler.markMainMenu() && StartupProfiler.shouldExitOnTitle()) {
             Minecraft.getInstance().stop();
         }
