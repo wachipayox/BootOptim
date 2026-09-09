7d9463e40aeff2038e8056f7fcce426e9bc710dd
Wachii <dani.alvarado.sosa@gmail.com>
2026-09-09 02:28:18 +0200

    Diagnostic: finish Decocraft baked output probe

--- src/main/java/dev/wachipayox/bootoptim/mixin/client/ModelBakeryDecocraftReuseMixin.java
+++ src/main/java/dev/wachipayox/bootoptim/mixin/client/ModelBakeryDecocraftReuseMixin.java
@@ -2,6 +2,7 @@ package dev.wachipayox.bootoptim.mixin.client;
 
 import dev.wachipayox.bootoptim.optimization.client.DecocraftPreparedGeometry;
 import dev.wachipayox.bootoptim.optimization.client.DecocraftRotatedQuadReuse;
+import dev.wachipayox.bootoptim.profiling.client.DecocraftBakedQuadEquivalenceProbe;
 import net.minecraft.client.resources.model.ModelBakery;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,5 +20,6 @@ abstract class ModelBakeryDecocraftReuseMixin {
     private void bootoptim$finishDecocraftReuse(ModelBakery.TextureGetter textureGetter, CallbackInfo ci) {
         DecocraftRotatedQuadReuse.finishModelBake();
         DecocraftPreparedGeometry.finishModelBake();
+        DecocraftBakedQuadEquivalenceProbe.finishModelBake();
     }
 }
