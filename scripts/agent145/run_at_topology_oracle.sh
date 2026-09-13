1e3b2289c2a4bf48662879dd601ca06de5bb0fe8
Wachii <dani.alvarado.sosa@gmail.com>
2026-09-13 15:52:44 +0200

    Fetch upstream version tags for exact-source oracle build

--- scripts/agent145/run_at_topology_oracle.sh
+++ scripts/agent145/run_at_topology_oracle.sh
@@ -9,8 +9,8 @@ WORK_DIR="${RUNNER_TEMP:-/tmp}/bootoptim-agent145-access-transformers"
 rm -rf "$WORK_DIR"
 git init -q "$WORK_DIR"
 git -C "$WORK_DIR" remote add origin https://github.com/neoforged/AccessTransformers.git
-git -C "$WORK_DIR" fetch -q --depth 1 origin "$UPSTREAM_COMMIT"
-git -C "$WORK_DIR" checkout -q FETCH_HEAD
+git -C "$WORK_DIR" fetch -q --tags origin
+git -C "$WORK_DIR" checkout -q "$UPSTREAM_COMMIT"
 
 test "$(git -C "$WORK_DIR" rev-parse HEAD)" = "$UPSTREAM_COMMIT"
 test "$(git -C "$WORK_DIR" rev-parse HEAD:src/main/java/net/neoforged/accesstransformer/parser/AccessTransformerList.java)" = "$EXPECTED_LIST_BLOB"
